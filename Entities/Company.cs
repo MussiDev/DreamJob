855df418e9a8d2a59c2d37273b0f2d70b80bc6ce
joakom24 <joakoomussi@gmail.com>
2024-05-05 20:35:41 -0300

    Agregar archivos de proyecto.

---
+++ Entities/Company.cs
@@ -0,0 +1,12 @@
+﻿namespace Dreamjob.entities
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Ubication { get; set; }
+        public int Employees { get; set; }
+    }
+}
+
---
+++ Entities/Job.cs
@@ -0,0 +1,12 @@
+﻿namespace DreamJob.entities
+{
+    public class Job
+    {
+        public DateTime CreatedAt { get; set; }
+        public string Position { get; set; }
+        public string Company { get; set; }
+        public string Description { get; set; }
+        public string SalaryRange { get; set; }
+        public string Place { get; set; }
+    }
+}
